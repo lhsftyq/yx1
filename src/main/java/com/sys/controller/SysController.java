2124c6c50b6f7681605d19fcad6956f2c64d6717
lhsf <371121962@qq.com>
2019-02-28 17:03:09 +0800

    0

--- src/main/java/com/sys/controller/SysController.java
+++ src/main/java/com/sys/controller/SysController.java
@@ -1,22 +1,32 @@
 package com.sys.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 import com.sys.entity.SysUser;
 import com.sys.service.SysUserService;
 
 @RestController
 @RequestMapping("/sys")
 public class SysController {
-
+	private Logger logger = LoggerFactory.getLogger(SysController.class);
 	@Autowired
 	private SysUserService sysUserService;
-	
+
+	@GetMapping("/user/userList")
+	public ModelAndView userList(ModelAndView view) {
+		view.setViewName("sys/user/userList");
+		return view;
+	}
+
 	@GetMapping("/user/register")
 	public int save() {
-		SysUser  sysUser  = new SysUser();
+		SysUser sysUser = new SysUser();
 		sysUser.setDeptId("1");
 		sysUser.setCreateTime("2019-02-28 14:04:11");
 		sysUser.setEmail("371121962@qq.com");
@@ -26,23 +36,24 @@ public class SysController {
 		sysUser.setStatus("0");
 		sysUser.setUserId(0);
 		sysUser.setUserName("linghushaofei");
-		
+
 		return sysUserService.sysUserSave(sysUser);
 	}
-	
+
 	@GetMapping("/user/select")
 	public SysUser select() {
-		SysUser  sysUser  = new SysUser();
+		logger.info("请求查询用户表数据");
+		SysUser sysUser = new SysUser();
 		sysUser.setUserId(1);
 		return sysUserService.sysUserSelect(sysUser);
 	}
-	
+
 	@GetMapping("/user/update")
 	public int updateUser() {
-		SysUser  sysUser  = new SysUser();
+		SysUser sysUser = new SysUser();
 		sysUser.setUserId(1);
 		sysUser.setCreateTime("20190228142500");
 		return sysUserService.sysUserUpdate(sysUser);
 	}
-	
+
 }
